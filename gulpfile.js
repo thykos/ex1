685242a82fd9cc09679dc48736559743e1b6b9ae
thykos <thykos1994@gmail.com>
2015-02-19 17:46:22 +0200

    beta

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,36 @@ var gulp = require('gulp'),
     minifyCSS = require('gulp-minify-css'),
     sass = require('gulp-sass'),
     rename = require('gulp-rename'),
-    jade = require('gulp-jade');
+    jade = require('gulp-jade'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
 
-gulp.task('default', function() {
-    gulp.src('css/*.css')
-        .pipe(concatCSS("bundle.css"))
-        .pipe(minifyCSS())
-        .pipe(rename('bundle.min.css'))
-        .pipe(gulp.dest('app/'));
-});
-
-gulp.task('watch', function(){
-    gulp.watch('css/*.css',['default'])
-    
-})
-
-gulp.task('sass',function(){
-    gulp.src('css/style.scss')
+gulp.task('sass', function () {
+    return gulp.src('css/*.scss')
         .pipe(sass())
-})
+        .pipe(gulp.dest('css'))
+        .pipe(reload({stream:true}));
+});
 
 gulp.task('jade2html',function(){
     gulp.src('*.jade')
     .pipe(jade())
     .pipe(gulp.dest(''))
 })
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('default', ['sass','jade2html','browser-sync'], function() {
+    gulp.src('css/*.css')
+        .pipe(concatCSS("bundle.css"))
+        .pipe(minifyCSS())
+        .pipe(rename('bundle.min.css'))
+        .pipe(gulp.dest('app/'));
+    gulp.watch("css/*.scss", ['sass']);
+});
